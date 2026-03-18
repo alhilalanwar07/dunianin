1c3125734c52de8201fa93ec50db15df06365dce
Alhilal Anwar <alhilalanwar07@gmail.com>
2026-03-18 20:38:41 +0800

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import { polyfill } from "mobile-drag-drop";
+import { scrollBehaviourDragImageTranslateOverride } from "mobile-drag-drop/scroll-behaviour";
+
+// Add polyfill for HTML5 drag and drop on mobile touch devices
+polyfill({
+    // use this to make use of the scroll behaviour
+    dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride
+});
+
+// prevent default panning when dragging on touch devices
+window.addEventListener('touchmove', function() {}, {passive: false});
