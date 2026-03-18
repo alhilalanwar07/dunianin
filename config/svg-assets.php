c4043afe9d435bb2b25d671ef28e92cc205fcb1c
Alhilal Anwar <alhilalanwar07@gmail.com>
2026-03-19 03:48:44 +0800

    update icon

--- config/svg-assets.php
+++ config/svg-assets.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    // Legacy local icon set.
     'assets' => [
         'apel',
         'pisang',
@@ -21,6 +22,31 @@ return [
     ],
 
     'ui' => [
+        'gembok',
+        'centang',
+        'mahkota',
+    ],
+
+    // SVGRepo-backed overrides.
+    // Put files in: resources/views/components/svg/svgrepo/{name}.blade.php
+    // When a file exists with the same name, it will be used first by <x-svg-icon>.
+    'svgrepo' => [
+        'apel',
+        'pisang',
+        'jeruk',
+        'mangga',
+        'semangka',
+        'kucing',
+        'anjing',
+        'sapi',
+        'ayam',
+        'ikan',
+        'burung',
+        'balon',
+        'bintang',
+        'hati',
+        'gelembung',
+        'keranjang',
         'gembok',
         'centang',
         'mahkota',
--- resources/views/components/svg-icon.blade.php
+++ resources/views/components/svg-icon.blade.php
@@ -1,11 +1,18 @@
 @php
     $iconName = trim($name);
-    $viewName = 'components.svg.' . $iconName;
-    $availableAssets = array_merge(config('svg-assets.assets', []), config('svg-assets.ui', []));
+    $svgrepoViewName = 'components.svg.svgrepo.' . $iconName;
+    $legacyViewName = 'components.svg.' . $iconName;
+    $availableAssets = array_merge(
+        config('svg-assets.assets', []),
+        config('svg-assets.ui', []),
+        config('svg-assets.svgrepo', [])
+    );
 @endphp
 
-@if (in_array($iconName, $availableAssets, true) && view()->exists($viewName))
-    @include($viewName)
+@if (in_array($iconName, $availableAssets, true) && view()->exists($svgrepoViewName))
+    @include($svgrepoViewName)
+@elseif (in_array($iconName, $availableAssets, true) && view()->exists($legacyViewName))
+    @include($legacyViewName)
 @else
     <svg {{ $attributes->merge(['viewBox' => '0 0 64 64', 'fill' => 'none', 'xmlns' => 'http://www.w3.org/2000/svg']) }}>
         <rect x="8" y="8" width="48" height="48" rx="10" stroke="currentColor" stroke-width="4" />
